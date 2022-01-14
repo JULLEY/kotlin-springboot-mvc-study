426361481406d05a9299b5543111ddc12a31d3c9
woong <ung91@yahoo.com>
2022-01-14 23:46:41 +0900

    init

---
+++ src/main/kotlin/com/programming/mvc/KotlinSpringbootMvcApplication.kt
@@ -0,0 +1,11 @@
+package com.programming.mvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootMvcApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringbootMvcApplication>(*args)
+}
---
+++ src/test/kotlin/com/programming/mvc/KotlinSpringbootMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.programming.mvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
