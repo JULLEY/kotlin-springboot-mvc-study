61337f039eda0ae1e6fdc42dce0a9283137c64e0
woong <ung91@yahoo.com>
2022-01-15 11:58:55 +0900

    GetApi done

--- src/main/kotlin/com/programming/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/programming/mvc/controller/get/GetApiController.kt
@@ -1,5 +1,6 @@
 package com.programming.mvc.controller.get
 
+import com.programming.mvc.model.http.UserRequest
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -8,26 +9,38 @@ internal class GetApiController {
 
     @GetMapping(path = ["/hello"])
     fun hello(): String{
-        return "hello kotlin";
+        return "hello kotlin"
     }
 
     @GetMapping("/get-mapping/path-variable/{name}/{age}")
     fun pathVariable(@PathVariable name: String, @PathVariable age:Int): String{
-        println("${name}, ${age}");
-        return name + " " + age;
+        println("${name}, ${age}")
+        return name + " " + age
     }
 
     @GetMapping("/get-mapping/path-variable2/{name}/{age}")
     fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") age:Int): String{
         var name = "kotlin"
 
-        println("${_name}, ${age}");
-        return _name + " " + age;
+        println("${_name}, ${age}")
+        return _name + " " + age
     }
 
-    @GetMapping("/get-mapping/queryParam")
+    @GetMapping("/get-mapping/query-param")
     fun queryParam(@RequestParam(value = "name") name: String, @RequestParam(value = "age") age:Int): String{
-        println("${name}, ${age}");
-        return name + " " + age;
+        println("${name}, ${age}")
+        return name + " " + age
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest{
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Object>): Map<String, Object> {
+        println(map)
+        return map
     }
 }
---
+++ src/main/kotlin/com/programming/mvc/model/http/UserRequest.kt
@@ -0,0 +1,8 @@
+package com.programming.mvc.model.http
+
+data class UserRequest (
+    var name: String?=null,
+    var age: Int?=null,
+    var email: String?=null,
+    var address: String?=null,
+)
