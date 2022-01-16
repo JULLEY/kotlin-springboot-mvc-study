309361f5aa6430df8e083437a90d22dcfe2449a1
woong <ung91@yahoo.com>
2022-01-16 12:38:46 +0900

    pageController

---
+++ src/main/kotlin/com/programming/mvc/controller/page/PageController.kt
@@ -0,0 +1,23 @@
+package com.programming.mvc.controller.page
+
+import com.programming.mvc.model.http.UserRequest
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class PageController {
+
+    @GetMapping("/main")
+    fun main(): String {
+        return "main.html"
+    }
+
+    @ResponseBody
+    @GetMapping("/test")
+    fun response(): UserRequest {
+        return UserRequest().apply {
+            this.name = "곽두팔"
+        }
+    }
+}
