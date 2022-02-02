1a47a49fe3dbadc286d9515d98eb9c24a7cc8406
woong <ung91@yahoo.com>
2022-02-02 20:36:59 +0900

    validation

--- src/main/kotlin/com/programming/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/com/programming/mvc/controller/put/PutApiController.kt
@@ -3,7 +3,11 @@ package com.programming.mvc.controller.put
 import com.programming.mvc.model.http.Result
 import com.programming.mvc.model.http.UserRequest
 import com.programming.mvc.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -20,36 +24,50 @@ class PutApiController {
     }
 
     @PutMapping("/put-mapping/object")
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        // 0. Response
-        return UserResponse().apply {
-            // 1. result
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "success"
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
+
+        if(bindingResult.hasErrors()){
+            // 500 error
+            var msg = StringBuilder()
+            bindingResult.allErrors.forEach{
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.toString() + " : " + message + "\n")
             }
-        }.apply {
-            // 2. description
-            this.description = "~~~~~~~~~~~~~~~~~~~~~"
-        }.apply {
-            // 3. user mutable list
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "leo"
-                this.age = 32
-                this.email = "leo@leo.com"
-                this.address = "leo address"
-                this.phoneNumber = "010-1234-1234"
-            })
-            userList.add(UserRequest().apply {
-                this.name = "dd"
-                this.age = 32
-                this.email = "dd@dd.com"
-                this.address = "dd address"
-                this.phoneNumber = "010-1234-5678"
-            })
-            this.user = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+
+        return ResponseEntity.ok("")
+
+        // 0. Response
+//        return UserResponse().apply {
+//            // 1. result
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "success"
+//            }
+//        }.apply {
+//            // 2. description
+//            this.description = "~~~~~~~~~~~~~~~~~~~~~"
+//        }.apply {
+//            // 3. user mutable list
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "leo"
+//                this.age = 32
+//                this.email = "leo@leo.com"
+//                this.address = "leo address"
+//                this.phoneNumber = "010-1234-1234"
+//            })
+//            userList.add(UserRequest().apply {
+//                this.name = "dd"
+//                this.age = 32
+//                this.email = "dd@dd.com"
+//                this.address = "dd address"
+//                this.phoneNumber = "010-1234-5678"
+//            })
+//            this.user = userList
+//        }
     }
 }
--- src/main/kotlin/com/programming/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/programming/mvc/model/http/UserRequest.kt
@@ -1,11 +1,24 @@
 package com.programming.mvc.model.http
 
+import javax.validation.constraints.*
+
 data class UserRequest (
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8)
     var name: String?=null,
+
+    @field:PositiveOrZero
     var age: Int?=null,
+
+    @field:Email    // 이메일형식 검증
     var email: String?=null,
+
+    @field:NotBlank // 공백검증
     var address: String?=null,
+
+    @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
     var phoneNumber: String?=null
+
     //    @JsonProperty("phone_number")
     //    var phoneNumber: String?=null
 )
