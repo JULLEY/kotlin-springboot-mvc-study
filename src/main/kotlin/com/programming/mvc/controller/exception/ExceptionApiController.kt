df55d7983ee9bb355b3f5439ed297c4aa56d889b
Hyunwoong Leo Choi <atom2882@gmail.com>
2022-07-23 21:08:37 +0900

    GlobalControllerAdvice

---
+++ src/main/kotlin/com/programming/mvc/controller/exception/ExceptionApiController.kt
@@ -0,0 +1,16 @@
+package com.programming.mvc.controller.exception
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/exception")
+class ExceptionApiController {
+
+    @GetMapping("/hello")
+    fun hello(){
+        val list = mutableListOf<String>()
+        val temp = list[0]
+    }
+}
