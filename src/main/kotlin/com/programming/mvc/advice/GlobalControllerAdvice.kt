6b989a65f3129137da579c35d3391a534ec7772d
Hyunwoong Leo Choi <atom2882@gmail.com>
2022-07-23 21:14:58 +0900

    GlobalControllerAdvice

--- src/main/kotlin/com/programming/mvc/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/programming/mvc/advice/GlobalControllerAdvice.kt
@@ -1,7 +1,20 @@
 package com.programming.mvc.advice
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    fun exception(e : RuntimeException): String{
+        return "Server Error ^_____^"
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e : IndexOutOfBoundsException): ResponseEntity<String>{
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("IndexOutOfBoundsException####")
+    }
 }
